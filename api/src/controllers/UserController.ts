731982a1f645c8ead9da6c2c95467892027dc636
Cesar Augusto <contatocsaugusto@gmail.com>
2021-02-24 19:43:48 -0300

    end of third class

---
+++ api/src/controllers/UserController.ts
@@ -0,0 +1,35 @@
+// ctrl + shift + o   ... organiza os imports
+import { Request, Response } from 'express'
+import { getCustomRepository } from 'typeorm'
+import { UsersRepository } from '../repositories/UsersRepository'
+
+class UserController {
+
+    async create(req: Request, res: Response) {
+        const {name, email} = req.body
+ 
+        const usersRepository = getCustomRepository(UsersRepository)
+
+        // SELECT * FROM USERS WHERE EMAIL = "EMAIL"
+        const usersAlreadyExists = await usersRepository.findOne({
+            email
+        })
+
+        if(usersAlreadyExists) {
+            return res.status(400).json({
+                error: "User already exists!",
+            })
+        }
+
+       const user =  usersRepository.create({
+        name, email
+       })
+       
+       await usersRepository.save(user)
+
+        return res.status(201).json(user)
+
+    }
+}
+
+export { UserController }
